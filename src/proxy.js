440c15bfd2e63aa0a77b35dace273e338b51a829
abedin-ansari <jainulansari579@gmail.com>
2024-08-31 23:56:24 +0530

    proxy new

--- src/proxy.js
+++ src/proxy.js
@@ -43,3 +43,49 @@ module.exports = async (req, res) => {
       .json({ error: "Failed to fetch data", message: error.message });
   }
 };
+
+// const fetch = require("node-fetch");
+
+// module.exports = async (req, res) => {
+//   const { url } = req.query;
+
+//   if (!url) {
+//     return res.status(400).json({ error: "Missing URL parameter" });
+//   }
+
+//   // Handle CORS preflight requests
+//   if (req.method === "OPTIONS") {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Content-Type, Authorization"
+//     );
+//     return res.status(200).end();
+//   }
+
+//   try {
+//     const response = await fetch(url);
+
+//     if (!response.ok) {
+//       throw new Error(`Failed to fetch data: ${response.statusText}`);
+//     }
+
+//     const data = await response.text(); // or response.json() if the response is JSON
+
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET");
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Content-Type, Authorization"
+//     );
+//     res.setHeader("Content-Type", response.headers.get("content-type"));
+
+//     res.status(200).send(data);
+//   } catch (error) {
+//     // Return JSON error message
+//     res
+//       .status(500)
+//       .json({ error: "Failed to fetch data", message: error.message });
+//   }
+// };
