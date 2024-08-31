fb6b22775da83a752788bec993ab091a26fec97f
abedin-ansari <jainulansari579@gmail.com>
2024-08-31 22:59:31 +0530

    Added proxy.js and updated vercel.json for CORS proxy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,24 @@
-export const CDN_url =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+const vercelProxy = "https://your-vercel-app-name.vercel.app/proxy?url=";
 
-export const Menu_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2599333&lng=77.412615&restaurantId=";
+export const CDN_url = `${vercelProxy}https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`;
 
-export const Body_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2599333&lng=77.412615&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const Menu_URL = `${vercelProxy}https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2599333&lng=77.412615&restaurantId=`;
+
+export const Body_URL = `${vercelProxy}https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2599333&lng=77.412615&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
 
 export const LOGO_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRWXV0h60iYP_ZXuFrctnjTlNvCu6AcHALfg&s";
+
+// export const CDN_url =
+//   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+// export const Menu_URL =
+//   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2599333&lng=77.412615&restaurantId=";
+
+// export const Body_URL =
+//   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2599333&lng=77.412615&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+// export const LOGO_URL =
+//   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRWXV0h60iYP_ZXuFrctnjTlNvCu6AcHALfg&s";
 
 // this is how we can export multiple files these are called "Named export import"
