0aad2c7940fc99eb1d3e99be8674c075b9ade654
abedin-ansari <jainulansari579@gmail.com>
2024-08-31 22:49:15 +0530

    userclass remove

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,10 @@
 import User from "./User";
-import UserClass from "./UserClass";
 
 const About = () => {
   return (
     <div className="about p-4 text-center">
       <h2 className="text-lg md:text-2xl">This is About section</h2>
-      <UserClass />
+      <User />
     </div>
   );
 };
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
-
 const User = ({ name, location }) => {
-
-    const [count] = useState(0);
-
   return (
-    <div className='user-class'>
-        <h1>Count :{count}</h1>
-        <h2>{name}</h2>
-        <h3>{location}</h3>
-        <h4>mob no. : 7392838298</h4>
+    <div className="user-class">
+      <h2>{name}</h2>
+      <h3>{location}</h3>
+      <h4>mob no. : 7392838298</h4>
     </div>
-  )
-}
+  );
+};
 
 export default User;
